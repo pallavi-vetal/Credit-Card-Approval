e4ae03fc4f1fdec34b175a1e6d4c391d23e7e145
Pallavi Vetal <vetalpallavi21@gmail.com>
2019-10-12 23:24:48 +0530

    Add files via upload

---
+++ FlaskApp.py
@@ -0,0 +1,317 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+from flask import Flask, flash , redirect, render_template , request, session, abort , Markup
+import os
+import pandas as pd
+import numpy as np
+import tensorflow as tf
+import keras
+from keras.models import load_model
+from keras import backend as K
+from werkzeug import secure_filename
+import json
+from scipy.stats import kurtosis, skew
+import csv
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+import pickle
+from werkzeug import secure_filename
+
+
+# In[2]:
+
+
+app = Flask(__name__)
+app.secret_key = os.urandom(12)
+
+dropdown_list = []
+dropdown_list_2 = []
+
+
+# In[3]:
+
+
+UPLOAD_FOLDER = './Uploads'
+ALLOWED_EXTENSIONS = set(['csv'])
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+
+# In[4]:
+
+
+@app.route('/upload')
+def upload_file():
+    dropdown_list.clear()
+    dropdown_list_2.clear()
+    return render_template('upload.html')
+
+
+# In[5]:
+
+
+@app.route('/uploader', methods = ['GET', 'POST'])
+def uploader_file():
+     if request.method == 'POST':
+        
+        if 'file' not in request.files:
+            flash('No file part')
+            print('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        print(file.filename)
+        if file.filename == '':
+            flash('No selected file')
+            print('No selected file')
+            return redirect(request.url)
+        if file :
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            fpath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+            uploadFileAndPredict(fpath)
+            with open(fpath) as file:
+                allRead = csv.reader(file, delimiter=',')
+                lineCount = 0
+                for row in allRead:
+                    if lineCount==0:
+                        lineCount=lineCount+1
+                    else:
+                        lineCount=lineCount+1
+                        dropdown_list_2.append((row[0]))
+            return render_template('Result.html',  dropdown_list_2=dropdown_list_2)
+
+
+# In[6]:
+
+
+@app.route('/input_percent' , methods = ['GET','POST'])
+def input_num():
+    x = request.form["in"]
+    fpath = os.path.join("default", "output.csv")
+    line = pd.read_csv(fpath).shape[0]
+    y = round((float(x)*line)/100)
+    print(line)
+    print(y)
+    ls = []
+    lschurn=[]
+    with open(fpath) as file:
+        allRead = csv.reader(file, delimiter=',')
+        lineCount = 0
+        for row in allRead:
+            if lineCount == 0:
+                lineCount += 1
+            elif lineCount <= y and lineCount != 0:
+                ls.append(row[0])
+                lschurn.append(row[16])
+                lineCount += 1
+    lss=list(map(lambda x: float(x*100),list(pd.read_csv(fpath)['Approved'][:y].copy())))  
+    print(lss)
+    print(lschurn)
+    return render_template('Percent.html', outList = ls, value_list=lschurn,values_res=lss )
+
+
+# In[7]:
+
+
+@app.route('/check/<string:dropdown>',methods=['POST','GET'])
+def specific(dropdown):
+    x = dropdown
+    x = search_default(x)
+    #Key,Male,Age,Debt,Married,BankCustomer,EducationLevel,Ethnicity,YearsEmployed,PriorDefault,Employed,
+    #CreditScore,DriversLicense,Citizen,ZipCode,Income,Approved
+
+    key  = x[0]
+    Male = x[1]
+    Age = x[2]
+    Married  = x[3]
+    Debt = x[4]
+    BankCustomer  = x[5]
+    EducationLevel  = x[6]
+    Ethnicity = x[7]
+    YearsEmployed  = x[8]
+    PriorDefault = x[9]
+    Employed = x[10]
+    CreditScore = x[11]
+    Income = x[15]    
+    x = x[16]
+    pred= float(x)*100
+    values = [pred]
+    x = float(x)*100
+    x = round(x,2)
+    
+    return render_template('Chart.html', key=key, 
+                           Male=Male, Age=Age, Married=Married, Debt=Debt, 
+                           BankCustomer=BankCustomer, EducationLevel=EducationLevel, Ethnicity=Ethnicity,
+                           YearsEmployed=YearsEmployed, PriorDefault=PriorDefault, 
+                           Employed=Employed, CreditScore = CreditScore,
+                           Income = Income, values = values,pred=x)
+
+
+# In[8]:
+
+
+def preprocess_data(fileInput):
+    train = pd.read_csv(fileInput)  
+    train.replace('?', np.NaN, inplace = True)
+    train['YearsEmployed'] = [x*100 for x in train['YearsEmployed']]
+    train['Age'] = train['Age'].astype('float64')
+    train['Age'].fillna((train['Age'].mean()), inplace=True)
+    train['Married'] = train['Married'].astype('category')
+    train['BankCustomer'] = train['BankCustomer'].astype('category')
+    train['EducationLevel'] = train['EducationLevel'].astype('category')
+    train['Ethnicity'] = train['Ethnicity'].astype('category')
+    train['PriorDefault'] = train['PriorDefault'].astype('category')
+    train['Employed'] = train['Employed'].astype('category')
+    train['DriversLicense'] = train['DriversLicense'].astype('category')
+    train['Citizen'] = train['Citizen'].astype('category')
+
+    cat_columns = train.select_dtypes(['category']).columns
+    train[cat_columns] = train[cat_columns].apply(lambda x: x.cat.codes)
+    train = train.drop(['ZipCode','Male'], axis=1)
+    X_test = train.iloc[:,1:].values
+
+    return X_test
+    
+    
+
+
+# In[9]:
+
+
+@app.route('/')
+def home():
+        return render_template('upload.html')
+
+
+# In[10]:
+
+
+def uploadFileAndPredict(filename):
+    K.clear_session()
+    dropdown_list_2.clear()
+    proceseed_data = preprocess_data(filename)
+    model = pickle.load(open('LRModel.pkl', 'rb'))
+    y_pred = model.predict_proba(proceseed_data)
+    df = pd.read_csv(filename)
+    df['Approved'] = [x[0] for x in y_pred]
+    df.set_index('Key', inplace=True)
+    df.sort_values('Approved', ascending=False, inplace=True)
+    fpathr = os.path.join("default", "output.csv")
+    df.to_csv(fpathr)  
+    return y_pred
+             
+    
+
+
+# In[11]:
+
+
+@app.route('/defaultfile', methods = ['GET', 'POST'])
+def uploader_default_file():
+    fpath = os.path.join("default", "test.csv")
+    uploadFileAndPredict(fpath)
+    with open(fpath) as file:
+        allRead = csv.reader(file, delimiter=',')
+        lineCount = 0
+        for row in allRead:
+            if lineCount==0:
+                lineCount=lineCount+1
+            else:
+                lineCount=lineCount+1
+                dropdown_list_2.append((row[0]))
+    return render_template('Result.html',  dropdown_list_2=dropdown_list_2)
+
+
+# In[12]:
+
+
+@app.route('/check_default/<string:dropdown_2>',methods=['POST','GET'])
+def specific_default(dropdown_2):
+    x = dropdown_2
+    
+    x = search_default(x)
+    key  = x[0]
+    Male = x[1]
+    Age = x[2]
+    Married  = x[3]
+    Debt = x[4]
+    BankCustomer  = x[5]
+    EducationLevel  = x[6]
+    Ethnicity = x[7]
+    YearsEmployed  = x[8]
+    PriorDefault = x[9]
+    Employed = x[10]
+    CreditScore = x[11]
+    Income = x[15]    
+    x = x[16]
+    pred= float(x)*100
+    values = [pred]
+    x = float(x)*100
+    x = round(x,2)
+    
+    return render_template('Chart.html', key=key, 
+                           Male=Male, Age=Age, Married=Married, Debt=Debt, 
+                           BankCustomer=BankCustomer, EducationLevel=EducationLevel, Ethnicity=Ethnicity,
+                           YearsEmployed=YearsEmployed, PriorDefault=PriorDefault, 
+                           Employed=Employed, CreditScore = CreditScore,
+                           Income = Income, values = values,pred=x)
+
+
+# In[13]:
+
+
+def search_default(cid):
+    fpathr = os.path.join("default", "output.csv")
+    with open(fpathr) as file:
+        allRead = csv.reader(file, delimiter=',')
+        for row in allRead:
+            if row[0]==cid:
+                return row
+
+
+# In[ ]:
+
+
+if __name__ == "__main__":
+    app.run()
+    
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# Credit-Card-Approval
+Credit Card
+
+Problem Statement
+The present generation is highly motivated towards the use of credit cards. There are a large number of applications for the credit card and it becomes very tedious for the bank to decide which person should be approved a credit card and which not. The list of defaulters is increasing and this brings trouble for the bank and other customers too. The bank needs ways to minimise this number for the proper functioning of the credit card feature and expand to a wider audience. It is where the role of Machine Learning and Data Science comes into play. The tedious task can be dealt with Machine Learning by training the models with minimized errors.
+
+Task
+The bank has provided the data of customers with certain attributes. You need to predict if the customer would be approved a credit card or not. If the credit card is approved, it is denoted with ‘+’ and if it is not approved then it is denoted with '-'.
+
